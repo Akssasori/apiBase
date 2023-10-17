f3ecefdc28d50a732dbca1355610f30733651698
Akssasori <diniz3003@gmail.com>
2023-10-17 08:55:53 -0300

    ajustes

--- src/main/java/com/redis/cache/service/impls/StudentServiceImpl.java
+++ src/main/java/com/redis/cache/service/impls/StudentServiceImpl.java
@@ -3,9 +3,6 @@ package com.redis.cache.service.impls;
 import com.redis.cache.model.Student;
 import com.redis.cache.repository.StudentRepository;
 import com.redis.cache.service.interfaces.StudentService;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,13 +16,11 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    @CacheEvict(cacheNames = "student", allEntries = true)
     public Student save(Student student) {
         return studentRepository.save(student);
     }
 
     @Override
-    @Cacheable(value = "student")
     public List<Student> findAll() {
         System.out.println("sem cache");
         return (List<Student>) studentRepository.findAll();
